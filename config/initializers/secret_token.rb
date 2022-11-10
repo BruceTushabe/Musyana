b9ff666fe21f7a29570ac47c78eb45fef6eb3269
Bruce Tushabe <brucetushabe@gmail.com>
2022-11-10 14:40:48 +0300

    Changed the tech stack to react

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Musyana::Application.config.secret_token = 'ee91c593e0b4b68958479a9a1fa62f14493bc116f1ac1e14174e24a47078c8b3a6fdd9fbebc97b43868dcc0cfaa5016d080be5e4320c747105a16d9111cdfc70'
